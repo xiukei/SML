a61c0bc6b3a79336b0b8d8d2c97d9ce5f800cc56
xiukei <741323066@qq.com>
2019-09-09 23:43:16 +1000

    fix something

--- prediction.py
+++ prediction.py
@@ -7,6 +7,7 @@ from sklearn.metrics import classification_report, confusion_matrix, accuracy_sc
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.preprocessing import LabelEncoder
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.preprocessing import StandardScaler, MinMaxScaler
 train = pd.read_csv('dataframe.csv')
 test = pd.read_csv('test_data.csv')
 # train = train[:10000]
@@ -18,29 +19,34 @@ X_test = test
 
 # X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
-# print (X_train)
-# print (y_train)
+print (X_train)
+print (y_train)
 # nb = MultinomialNB()
 # nb.fit(X_train, y_train)
 # y_pred = nb.predict(X_test)
 # for data in y_pred:
 #     print(data)
-svc = SVC(kernel='linear')
-svc.fit(X_train, y_train)
-y_pred = svc.predict(X_test)
+# svc = SVC(kernel='poly', gamma='scale', max_iter=1000)
+# svc.fit(X_train, y_train)
+# print('hello')
+# y_pred = svc.predict(X_test)
 
-# rfc = RandomForestClassifier(n_estimators=100)
-# rfc.fit(X_train, y_train)
-# y_pred = rfc.predict(X_test)
-prediction_columns = ['prediction']
-try:
-    with open('prediction.csv', 'wb') as f:
-        writer = csv.DictWriter(f, fieldnames=prediction_columns)
-        writer.writeheader()
-        for row in y_pred:
-            writer.writerow(row)
-except IOError as (errno, strerror):
-    print('I/O error({0}): {1}'.format(errno, strerror))
+rfc = RandomForestClassifier(n_estimators=50, random_state=120)
+rfc.fit(X_train, y_train)
+y_pred = rfc.predict(X_test)
+print(y_pred)
+prediction_columns = ['Id', 'Predicted']
+# try:
+#     with open('prediction.csv', 'wb') as f:
+#         writer = csv.DictWriter(f, fieldnames=prediction_columns)
+#         writer.writeheader()
+#         for row in y_pred:
+#             writer.writerow(row)
+# except IOError as (errno, strerror):
+#     print('I/O error({0}): {1}'.format(errno, strerror))
+output = pd.DataFrame({'Id': range(1, y_pred.shape[0] + 1),
+                       'Predicted': y_pred})
+output.to_csv('./prediction.csv', index=False)
 # print('accuracy %s' % accuracy_score(y_pred, y_test))
 # print(confusion_matrix(y_test, y_pred))
 # print(classification_report(y_test, y_pred))
