7778f000c74f2f47a385503b6ccaf5ca996faf0d
xiukei <741323066@qq.com>
2019-09-11 00:14:42 +1000

    change dataset and test with different methods

--- prediction.py
+++ prediction.py
@@ -1,5 +1,4 @@
 from sklearn.svm import SVC
-import numpy as np
 import pandas as pd
 import csv
 from sklearn.model_selection import train_test_split
@@ -8,45 +7,65 @@ from sklearn.naive_bayes import MultinomialNB
 from sklearn.preprocessing import LabelEncoder
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.preprocessing import StandardScaler, MinMaxScaler
+from sklearn.linear_model import LogisticRegression
+from sklearn import tree
+from sklearn.neighbors import KNeighborsClassifier
 train = pd.read_csv('dataframe.csv')
 test = pd.read_csv('test_data.csv')
-# train = train[:10000]
-y_train = train.author
-X_train = train.drop('author', axis=1)
-X_test = test
-# print (X)
-# print (y)
+train = train[:5000]
+y = train.author
+X = train.drop('author', axis=1)
+# X_test = test
 
-# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
-print (X_train)
-print (y_train)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.4)
+
+# print(X_train)
+# print(y_train)
+
+# Naive Bayes
 # nb = MultinomialNB()
 # nb.fit(X_train, y_train)
 # y_pred = nb.predict(X_test)
-# for data in y_pred:
-#     print(data)
-# svc = SVC(kernel='poly', gamma='scale', max_iter=1000)
-# svc.fit(X_train, y_train)
-# print('hello')
-# y_pred = svc.predict(X_test)
 
-rfc = RandomForestClassifier(n_estimators=50, random_state=120)
-rfc.fit(X_train, y_train)
-y_pred = rfc.predict(X_test)
-print(y_pred)
-prediction_columns = ['Id', 'Predicted']
-# try:
-#     with open('prediction.csv', 'wb') as f:
-#         writer = csv.DictWriter(f, fieldnames=prediction_columns)
-#         writer.writeheader()
-#         for row in y_pred:
-#             writer.writerow(row)
-# except IOError as (errno, strerror):
-#     print('I/O error({0}): {1}'.format(errno, strerror))
-output = pd.DataFrame({'Id': range(1, y_pred.shape[0] + 1),
-                       'Predicted': y_pred})
-output.to_csv('./prediction.csv', index=False)
-# print('accuracy %s' % accuracy_score(y_pred, y_test))
-# print(confusion_matrix(y_test, y_pred))
-# print(classification_report(y_test, y_pred))
+# Support Vector Machine
+svc = SVC(kernel='poly')
+svc.fit(X_train, y_train)
+print('hello')
+y_pred = svc.predict(X_test)
+
+# RandomForestClassifier()
+# rfc = RandomForestClassifier(n_estimators=100, n_jobs=2)
+# rfc.fit(X_train, y_train)
+# y_pred = rfc.predict(X_test)
+
+# ssCoder = StandardScaler()
+# X_ss = ssCoder.fit_transform(X)
+# X = pd.DataFrame(X_ss)
+#
+# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
+#
+# # LogisticRegression()
+# clf = LogisticRegression()
+# clf.fit(X_train, y_train)
+# y_pred = clf.predict(X_test)
+
+# DecisionTreeClassifier
+# clf = tree.DecisionTreeClassifier(criterion='entropy')
+# clf.fit(X_train, y_train)
+# y_pred = clf.predict(X_test)
+
+# KNeighborsClassifier
+# knn_clf = KNeighborsClassifier()
+# knn_clf.fit(X_train, y_train)
+# y_pred = knn_clf.predict(X_test)
+# print(y_pred)
+
+# Output results into csv file
+# prediction_columns = ['Id', 'Predicted']
+# output = pd.DataFrame({'Id': range(1, y_pred.shape[0] + 1),
+#                        'Predicted': y_pred})
+# output.to_csv('./prediction.csv', index=False)
+print('accuracy %s' % accuracy_score(y_pred, y_test))
+print(confusion_matrix(y_test, y_pred))
+print(classification_report(y_test, y_pred))
--- retrieve_features.py
+++ retrieve_features.py
@@ -1,154 +1,158 @@
 import re
 import csv
+from nltk.corpus import stopwords
 train_tweets = 'train_tweets.txt'
 test_tweets = 'test_tweets_unlabeled.txt'
 tweets = []
 authors = []
 data_frame = {}
 
-
-with open(train_tweets) as f:
-    line = f.readline()
-    while line:
-        author = line.split('\t')[0]
-        line = line.strip()
-
-        if author not in authors:
-            authors.append(author)
-        tweet = line.split('\t')[1]
-        tweet = tweet.lower()
-        tweet = re.sub(r'(http|https|ftp)://[a-zA-Z0-9\\./]+', '', tweet)
-        # tweet = re.sub(r'@(\w+)', '', tweet)
-        # tweet = re.sub(r'#(\w+)', '', tweet)
-        if author in data_frame.keys():
-            # print(author)
-            data_frame[author]['word_count'] += len(tweet.split())
-            data_frame[author]['character_count'] += len(tweet)
-            data_frame[author]['tweet_count'] += 1
-            data_frame[author]['digit_count'] += sum(c.isdigit() for c in tweet)
-            data_frame[author]['colon_count'] += sum(c == ':' for c in tweet)
-            data_frame[author]['semicolon_count'] += sum(c == ';' for c in tweet)
-            data_frame[author]['qmark_count'] += sum(c == '?' for c in tweet)
-            data_frame[author]['period_count'] += sum(c == ',' for c in tweet)
-            data_frame[author]['comma_count'] += sum(c == '.' for c in tweet)
-            data_frame[author]['exclamation_count'] += sum(c == '!' for c in tweet)
-            data_frame[author]['@_count'] += sum(c == '@' for c in tweet)
-            data_frame[author]['#_count'] += sum(c == '#' for c in tweet)
-        else:
-            # print(author)
-            data_frame[author] = {}
-            data_frame[author]['word_count'] = len(tweet.split())
-            data_frame[author]['character_count'] = len(tweet)
-            data_frame[author]['tweet_count'] = 1
-            data_frame[author]['digit_count'] = sum(c.isdigit() for c in tweet)
-            data_frame[author]['colon_count'] = sum(c == ':' for c in tweet)
-            data_frame[author]['semicolon_count'] = sum(c == ';' for c in tweet)
-            data_frame[author]['qmark_count'] = sum(c == '?' for c in tweet)
-            data_frame[author]['period_count'] = sum(c == ',' for c in tweet)
-            data_frame[author]['comma_count'] = sum(c == '.' for c in tweet)
-            data_frame[author]['exclamation_count'] = sum(c == '!' for c in tweet)
-            data_frame[author]['@_count'] = sum(c == '@' for c in tweet)
-            data_frame[author]['#_count'] = sum(c == '#' for c in tweet)
-
-        # print(data_frame)
-        line = f.readline()
-    f.close()
-
-# # print(data_frame)
-for key in data_frame:
-    data_frame[key]['avg_word'] = data_frame[key]['word_count'] / data_frame[key]['tweet_count']
-    data_frame[key]['avg_character'] = data_frame[key]['character_count'] / data_frame[key]['tweet_count']
-    data_frame[key]['avg_digit'] = data_frame[key]['digit_count'] / data_frame[key]['tweet_count']
-    data_frame[key]['avg_colon'] = data_frame[key]['colon_count'] / data_frame[key]['tweet_count']
-    data_frame[key]['avg_semicolon'] = data_frame[key]['semicolon_count'] / data_frame[key]['tweet_count']
-    data_frame[key]['avg_qmark'] = data_frame[key]['qmark_count'] / data_frame[key]['tweet_count']
-    data_frame[key]['avg_period'] = data_frame[key]['period_count'] / data_frame[key]['tweet_count']
-    data_frame[key]['avg_comma'] = data_frame[key]['comma_count'] / data_frame[key]['tweet_count']
-    data_frame[key]['avg_exclamation'] = data_frame[key]['exclamation_count'] / data_frame[key]['tweet_count']
-    data_frame[key]['avg_@'] = data_frame[key]['@_count'] / data_frame[key]['tweet_count']
-    data_frame[key]['avg_#'] = data_frame[key]['#_count'] / data_frame[key]['tweet_count']
-csv_data = []
-
-for key in data_frame:
-    row = {'author': key, 'word': data_frame[key]['avg_word'], 'character': data_frame[key]['avg_character'],
-           'digit': data_frame[key]['avg_digit'],
-           'colon': data_frame[key]['avg_colon'], 'semicolon': data_frame[key]['avg_semicolon'],
-           'qmark': data_frame[key]['avg_qmark'], 'period': data_frame[key]['avg_period'],
-           'comma': data_frame[key]['avg_comma'], 'exclamation': data_frame[key]['avg_exclamation'],
-           '@': data_frame[key]['avg_@'], '#': data_frame[key]['avg_#']}
-    csv_data.append(row)
-
-train_columns = ['author', 'word', 'character', 'digit', 'colon', 'semicolon', 'qmark', 'period', 'comma', 'exclamation',
-                 '@', '#']
-csv_columns = ['author', 'word_count', 'character_count', 'digit_count', 'colon_count', 'semicolon_count', 'qmark_count',
-               'period_count', 'comma_count', 'exclamation_count', '@_count', '#_count']
-test_columns = ['word', 'character', 'digit', 'colon', 'semicolon', 'qmark',
-                'period', 'comma', 'exclamation', '@', '#']
-
-try:
-    with open('dataframe.csv', 'wb') as file:
-        writer = csv.DictWriter(file, fieldnames=train_columns)
-        writer.writeheader()
-
-        for row in csv_data:
-            writer.writerow(row)
-except IOError as (errno, strerror):
-    print('I/O error({0}): {1}'.format(errno, strerror))
-#
-# df = []
+sw = stopwords.words
 # with open(train_tweets) as f:
 #     line = f.readline()
 #     while line:
 #         author = line.split('\t')[0]
 #         line = line.strip()
+#
 #         if author not in authors:
 #             authors.append(author)
 #         tweet = line.split('\t')[1]
 #         tweet = tweet.lower()
 #         tweet = re.sub(r'(http|https|ftp)://[a-zA-Z0-9\\./]+', '', tweet)
-#         row = {'author': author, 'word_count': len(tweet.split()), 'character_count': len(tweet),
-#                 'digit_count': sum(c.isdigit() for c in tweet), 'colon_count': sum(c == ':' for c in tweet),
-#                 'semicolon_count': sum(c == ';' for c in tweet), 'qmark_count': sum(c == '?' for c in tweet),
-#                 'period_count': sum(c == ',' for c in tweet), 'comma_count': sum(c == '.' for c in tweet),
-#                 'exclamation_count': sum(c == '!' for c in tweet), '@_count': sum(c == '@' for c in tweet),
-#                 '#_count': sum(c == '#' for c in tweet)}
-#         df.append(row)
+#         # tweet = re.sub(r'@(\w+)', '', tweet)
+#         # tweet = re.sub(r'#(\w+)', '', tweet)
+#         if author in data_frame.keys():
+#             # print(author)
+#             data_frame[author]['word_count'] += len(tweet.split())
+#             data_frame[author]['character_count'] += len(tweet)
+#             data_frame[author]['tweet_count'] += 1
+#             data_frame[author]['digit_count'] += sum(c.isdigit() for c in tweet)
+#             data_frame[author]['colon_count'] += sum(c == ':' for c in tweet)
+#             data_frame[author]['semicolon_count'] += sum(c == ';' for c in tweet)
+#             data_frame[author]['qmark_count'] += sum(c == '?' for c in tweet)
+#             data_frame[author]['period_count'] += sum(c == ',' for c in tweet)
+#             data_frame[author]['comma_count'] += sum(c == '.' for c in tweet)
+#             data_frame[author]['exclamation_count'] += sum(c == '!' for c in tweet)
+#             data_frame[author]['@_count'] += sum(c == '@' for c in tweet)
+#             data_frame[author]['#_count'] += sum(c == '#' for c in tweet)
+#         else:
+#             # print(author)
+#             data_frame[author] = {}
+#             data_frame[author]['word_count'] = len(tweet.split())
+#             data_frame[author]['character_count'] = len(tweet)
+#             data_frame[author]['tweet_count'] = 1
+#             data_frame[author]['digit_count'] = sum(c.isdigit() for c in tweet)
+#             data_frame[author]['colon_count'] = sum(c == ':' for c in tweet)
+#             data_frame[author]['semicolon_count'] = sum(c == ';' for c in tweet)
+#             data_frame[author]['qmark_count'] = sum(c == '?' for c in tweet)
+#             data_frame[author]['period_count'] = sum(c == ',' for c in tweet)
+#             data_frame[author]['comma_count'] = sum(c == '.' for c in tweet)
+#             data_frame[author]['exclamation_count'] = sum(c == '!' for c in tweet)
+#             data_frame[author]['@_count'] = sum(c == '@' for c in tweet)
+#             data_frame[author]['#_count'] = sum(c == '#' for c in tweet)
+#
+#         # print(data_frame)
 #         line = f.readline()
 #     f.close()
 #
+# # # print(data_frame)
+# for key in data_frame:
+#     data_frame[key]['avg_word'] = data_frame[key]['word_count'] / data_frame[key]['tweet_count']
+#     data_frame[key]['avg_character'] = data_frame[key]['character_count'] / data_frame[key]['tweet_count']
+#     data_frame[key]['avg_digit'] = data_frame[key]['digit_count'] / data_frame[key]['tweet_count']
+#     data_frame[key]['avg_colon'] = data_frame[key]['colon_count'] / data_frame[key]['tweet_count']
+#     data_frame[key]['avg_semicolon'] = data_frame[key]['semicolon_count'] / data_frame[key]['tweet_count']
+#     data_frame[key]['avg_qmark'] = data_frame[key]['qmark_count'] / data_frame[key]['tweet_count']
+#     data_frame[key]['avg_period'] = data_frame[key]['period_count'] / data_frame[key]['tweet_count']
+#     data_frame[key]['avg_comma'] = data_frame[key]['comma_count'] / data_frame[key]['tweet_count']
+#     data_frame[key]['avg_exclamation'] = data_frame[key]['exclamation_count'] / data_frame[key]['tweet_count']
+#     data_frame[key]['avg_@'] = data_frame[key]['@_count'] / data_frame[key]['tweet_count']
+#     data_frame[key]['avg_#'] = data_frame[key]['#_count'] / data_frame[key]['tweet_count']
+# csv_data = []
 #
+# for key in data_frame:
+#     row = {'author': key, 'word': data_frame[key]['avg_word'], 'character': data_frame[key]['avg_character'],
+#            'digit': data_frame[key]['avg_digit'],
+#            'colon': data_frame[key]['avg_colon'], 'semicolon': data_frame[key]['avg_semicolon'],
+#            'qmark': data_frame[key]['avg_qmark'], 'period': data_frame[key]['avg_period'],
+#            'comma': data_frame[key]['avg_comma'], 'exclamation': data_frame[key]['avg_exclamation'],
+#            '@': data_frame[key]['avg_@'], '#': data_frame[key]['avg_#']}
+#     csv_data.append(row)
+
+train_columns = ['author', 'word', 'character', 'digit', 'avg_words', 'stopwords', 'uppercase', '@', '#', 'retweet', 'link']
+# csv_columns = ['author', 'word_count', 'character_count', 'digit_count', 'colon_count', 'semicolon_count', 'qmark_count',
+#                'period_count', 'comma_count', 'exclamation_count', '@_count', '#_count']
+test_columns = ['word', 'character', 'digit', 'avg_words', 'stopwords', 'uppercase', '@', '#', 'retweet', 'link']
 # try:
 #     with open('dataframe.csv', 'wb') as file:
-#         writer = csv.DictWriter(file, fieldnames=csv_columns)
+#         writer = csv.DictWriter(file, fieldnames=train_columns)
 #         writer.writeheader()
-#         for row in df:
+#
+#         for row in csv_data:
 #             writer.writerow(row)
 # except IOError as (errno, strerror):
 #     print('I/O error({0}): {1}'.format(errno, strerror))
 
 
+def avg_words(ln):
+    words = ln.split()
+    return sum(len(word) for word in words) / len(words)
+
+
+# df = []
+# with open(train_tweets, encoding='utf-8') as f:
+#     line = f.readline()
+#     while line:
+#         line = line.strip()
+#         author = line.split('\t')[0]
+#         tweet = line.split('\t')[1]
+#         # tweet = tweet.lower()
+#         # tweet = re.sub(r'(http|https|ftp)://[a-zA-Z0-9\\./]+', '', tweet)
+#         row = {'author': author, 'word': len(tweet.split()), 'character': len(tweet), 'digit': sum(c.isdigit() for c in tweet),
+#                'avg_words': avg_words(tweet), 'stopwords': len([word for word in tweet.lower().split() if word in stopwords.words('english')]),
+#                'uppercase': len([word for word in tweet.split() if word.isupper()]), '@': sum(c == '@' for c in tweet),
+#                '#': sum(c == '#' for c in tweet), 'retweet': len([word for word in tweet.split() if word in ['RT', 'rt']]),
+#                'link': len([word for word in tweet.split() if word.startswith(('http', 'https', 'ftp'))])}
+#         df.append(row)
+#         print(row)
+#         line = f.readline()
+#     f.close()
+#
+#
+# try:
+#     with open('dataframe.csv', 'w', newline='', encoding='utf-8') as f:
+#         writer = csv.DictWriter(f, fieldnames=train_columns)
+#         writer.writeheader()
+#         for row in df:
+#             writer.writerow(row)
+#         f.close()
+# except IOError:
+#     print('Opps')
+
+
 test_data = []
-with open(test_tweets) as f:
+with open(test_tweets, encoding='utf-8') as f:
     line = f.readline()
     while line:
-        line = line.strip()
-        tweet = line.lower()
-        tweet = re.sub(r'(http|https|ftp)://[a-zA-Z0-9\\./]+', '', tweet)
-        row = { 'word': len(tweet.split()), 'character': len(tweet),
-                'digit': sum(c.isdigit() for c in tweet), 'colon': sum(c == ':' for c in tweet),
-                'semicolon': sum(c == ';' for c in tweet), 'qmark': sum(c == '?' for c in tweet),
-                'period': sum(c == ',' for c in tweet), 'comma': sum(c == '.' for c in tweet),
-                'exclamation': sum(c == '!' for c in tweet), '@': sum(c == '@' for c in tweet),
-                '#': sum(c == '#' for c in tweet)}
+        tweet = line.strip()
+        # tweet = line.lower()
+        # tweet = re.sub(r'(http|https|ftp)://[a-zA-Z0-9\\./]+', '', tweet)
+        row = {'word': len(tweet.split()), 'character': len(tweet), 'digit': sum(c.isdigit() for c in tweet),
+               'avg_words': avg_words(tweet), 'stopwords': len([word for word in tweet.lower().split() if word in stopwords.words('english')]),
+               'uppercase': len([word for word in tweet.split() if word.isupper()]), '@': sum(c == '@' for c in tweet),
+               '#': sum(c == '#' for c in tweet), 'retweet': len([word for word in tweet.split() if word in ['RT', 'rt']]),
+               'link': len([word for word in tweet.split() if word.startswith(('http', 'https', 'ftp'))])}
         test_data.append(row)
+        print(row)
         line = f.readline()
     f.close()
 
 try:
-    with open('test_data.csv', 'wb') as f:
+    with open('test_data.csv', 'w', newline='', encoding='utf-8') as f:
         writer = csv.DictWriter(f, fieldnames=test_columns)
         writer.writeheader()
         for row in test_data:
             writer.writerow(row)
-except IOError as (errno, strerror):
-    print('I/O error({0}): {1}'.format(errno, strerror))
+        f.close()
+except IOError:
+    print('Opps')
